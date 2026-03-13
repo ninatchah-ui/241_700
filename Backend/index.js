789381d2b212c391d89db8e6c95c75b381113aee
ninatchah-ui <ninatcha.h@ku.th>
2026-03-13 11:48:40 +0700

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -89,18 +89,126 @@ app.get('/users',async(req,res)=>{
     res.json(results[0]);
 })
 
+const validateData = (userData) => {
+    let error = [];
+
+    if (!userData.firstName) {
+        error.push('กรุณากรอกชื่อ');
+    }
+
+    if (!userData.lastName) {
+        error.push('กรุณากรอกนามสกุล');
+    }
+
+    if (!userData.age) {
+        error.push('กรุณากรอกอายุ');
+    }
+
+    if (!userData.gender) {
+        error.push('กรุณาเลือกเพศ');
+    }
+
+    if (!userData.interests) {
+        error.push('กรุณาเลือกงานอดิเรก');
+    }
+
+    if (!userData.description) {
+        error.push('กรุณากรอกคำอธิบาย');
+    }
+
+    return error;
+};
+
+const submitData = async () => {
+
+    const firstNameDOM = document.querySelector('input[name=firstname]');
+    const lastNameDOM = document.querySelector('input[name=lastname]');
+    const ageDOM = document.querySelector('input[name=age]');
+    const genderDOM = document.querySelector('input[name=gender]:checked');
+    const interestDOMs = document.querySelectorAll('input[name=interests]:checked');
+    const descriptionDOM = document.querySelector('textarea[name=description]');
+    const messageDOM = document.getElementById('message');
+
+    try {
+
+        // รวม interests
+        let interests = Array.from(interestDOMs).map(el => el.value).join(',');
+
+        const userData = {
+            firstName: firstNameDOM.value,
+            lastName: lastNameDOM.value,
+            age: ageDOM.value,
+            gender: genderDOM ? genderDOM.value : '',
+            description: descriptionDOM.value,
+            interests: interests
+        };
+
+        console.log('submitData', userData);
+
+        const errors = validateData(userData);
+
+        if (errors.length > 0) {
+            throw {
+                message: 'กรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            };
+        }
+
+        const response = await axios.post('http://localhost:8000/users', userData);
+
+        console.log('response', response);
+
+        messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ';
+        messageDOM.className = 'message success';
+
+    } catch (error) {
+
+        console.log('error message:', error.message);
+
+        let htmlData = '<div>';
+        htmlData += `<p>${error.message || 'เกิดข้อผิดพลาด'}</p>`;
+
+        if (Array.isArray(error.errors)) {
+            htmlData += '<ul>';
+
+            error.errors.forEach(err => {
+                htmlData += `<li>${err}</li>`;
+            });
+
+            htmlData += '</ul>';
+        }
+
+        htmlData += '</div>';
+
+        messageDOM.innerHTML = htmlData;
+        messageDOM.className = 'message danger';
+    }
+};
+
 // path: = POST /users สำหรับเพิ่มข้อมูล user ใหม่
 app.post('/users',async(req,res)=>{
     try{
         let user = req.body;
+        const errors = validateData(user);
+        if(error.lenght>0) {
+            throw {
+                message : 'กรุณากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }
         const results = await conn.query('INSERT INTO users SET ?',user);
         res.json({
         massage: 'User added successfully',
         data: results[0]
     });
     }catch(error){
+        const errorMessage = error.message || 'Error adding user';
+        const errors = error.errors || [];
         console.error('Error inserting user:',error);
-        res.status(500).json({error:'Error adding user'});
+        res.status(500).json({
+            message: errorMessage,
+            errors: errors
+        });
     }
 })
 
